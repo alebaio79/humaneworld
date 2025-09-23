ad9be63d3f0831b635bc305a40a1b4520949cd10
Alessandro <alessandro@filarete.it>
2025-09-23 10:03:22 +0200

    custom-card-style-script.js

---
+++ filarete/engaging-networks/js/custom-card-style-script.js
@@ -0,0 +1,58 @@
+	window.enVGSFields = {
+            'transaction.ccnumber': {
+                css: {
+                    'color': '#1b1d1f',
+                    'border': 'solid 1px #1b1d1f',
+                    'border-radius':'5px',
+                    'transition': 'border-color .15s ease-in-out',
+                    'padding': '0 10px',
+                    'box-sizing': 'border-box',
+                    '&:focus': {
+                        'border-color': '#11bef5',
+                    },
+                    '&.invalid.touched': {
+                        'color': 'red',
+                    },
+                    '&.valid': {
+                        'color': 'green',
+                    },
+                    '@font-face': {
+                        'font-family': 'freight-sans-pro',
+                        'font-style': 'normal',
+                        'font-weight': '400',
+                        'font-display': 'swap',
+                        'src': 'url("https://use.typekit.net/af/442215/000000000000000000010b5a/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2")',
+                    },
+                    'font-family': 'freight-sans-pro, monospace',
+                    'font-size': '100%',
+                },
+            },
+            'transaction.ccvv': {
+                css: {
+                    'color': '#1b1d1f',
+                    'border': 'solid 1px #1b1d1f',
+                    'border-radius':'5px',
+                    'transition': 'border-color .15s ease-in-out',
+                    'padding': '0 10px',
+                    'box-sizing': 'border-box',
+                    '&:focus': {
+                        'border-color': '#11bef5',
+                    },
+                    '&.invalid.touched': {
+                        'color': 'red',
+                    },
+                    '&.valid': {
+                        'color': 'green',
+                    },
+                    '@font-face': {
+                        'font-family': 'freight-sans-pro',
+                        'font-style': 'normal',
+                        'font-weight': '400',
+                        'font-display': 'swap',
+                        'src': 'url("https://use.typekit.net/af/442215/000000000000000000010b5a/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2")',
+                    },
+                    'font-family': 'freight-sans-pro, monospace',
+                    'font-size': '100%',
+                },
+            },
+        }
